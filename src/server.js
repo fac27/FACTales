72a4766537fc1a0a199ab1f09d03c198ba8335e8
FomasTreeman <tom@team-freeman.com>
2023-05-23 16:56:02 +0100

    include hadnler in server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const books = require('./routes/books.js');
+const movies = require('./routes/movies.js');
 const server = express();
 server.use(express.static('public'));
 
 server.post('/book', books.post);
+server.post('/movie', movies.post);
 
 module.exports = server;
