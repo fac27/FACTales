7ed37251397d8ee8251cad2bdcf555bc64be68d9
simonryrie <simonryrie@gmail.com>
2023-05-24 15:23:49 +0100

    Edited db.js to direct to volumes

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,13 @@
 const { readFileSync } = require("node:fs");
+const path = require("path");
 const Database = require("better-sqlite3");
 
-const db = new Database(process.env.DB_FILE);
+const rootDir = path.join(__dirname, "../..");
+const dbFilePath = path.join(rootDir, process.env.DB_FILE);
+const db = new Database(dbFilePath);
 
-const schema = readFileSync('src/database/schema.sql', "utf-8");
+const schemaFilePath = path.join(rootDir, "src", "database", "schema.sql");
+const schema = readFileSync(schemaFilePath, "utf-8");
 db.exec(schema);
 
 module.exports = db;
