16c27116303033c971a744d45ee2c704269f2026
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-05-24 16:17:36 +0100

    edited tests.js to test inserts, selects and viewRecommendations()

--- test/test-insert.js
+++ test/test-insert.js
@@ -3,16 +3,56 @@ const { assert } = require("node:assert");
 const { insertMovie } = require("../src/model/movies.js");
 const { movies } = require("../src/routes/movies.js");
 
-function testInsert() {
+function testInserts() {
   reset();
-  const newMovie = insertMovie(
-    "Alphonso",
-    "Crimes of the past",
-    "David Cronenberg"
+
+  movies.insertMovie("Dave", "WILLOW", "Someone");
+  movies.insertMovie("Jeoff", "Fast X", "Someone else");
+  movies.insertMovie("Chanelle", "Shrek", "Someone different");
+  movies.insertMovie("Gabrielle", "Iron Man", "Someone nonexistent");
+
+
+  const moviesList = model.selectMovieRecs();
+
+  movies.insertMovie("Dave", "WILLOW", "Someone");
+  movies.insertMovie("Jeoff", "Fast X", "Someone else");
+  movies.insertMovie("Chanelle", "Shrek", "Someone different");
+  movies.insertMovie("Gabrielle", "Iron Man", "Someone nonexistent");
+
+  assert.deepEqual(moviesList[0].user_name, "Dave", "found daves movie");
+  assert.deepEqual(
+    moviesList[0].director,
+    "Someone",
+    "found daves movies director"
   );
-  const body = movies.get().body();
-  console.log(newMovie)
-  assert.match(body, newMovie);
 }
 
-module.exports = { testInsert };
+function testSelects() {
+  reset();
+
+  movies.insertMovie("Dave", "WILLOW", "Someone");
+  movies.insertMovie("Jeoff", "Fast X", "Someone else");
+  movies.insertMovie("Chanelle", "Shrek", "Someone different");
+  movies.insertMovie("Gabrielle", "Iron Man", "Someone nonexistent");
+
+  const moviesList = model.selectMovieRecs();
+  assert.deepEqual(moviesList.length, 4);
+}
+
+function testViewRecs() {
+  reset();
+
+  movies.insertMovie("Dave", "WILLOW", "Someone");
+  movies.insertMovie("Jeoff", "Fast X", "Someone else");
+  movies.insertMovie("Chanelle", "Shrek", "Someone different");
+  movies.insertMovie("Gabrielle", "Iron Man", "Someone nonexistent");
+
+  const htmlList = viewRecommendations();
+  const daveExists = htmlList.find("Dave");
+  const shrekExists = htmlList.find("Shrek");
+
+  assert.deepEqual(daveExists, true, "html included dave");
+  assert.deepEqual(shrekExists, true, "html included shrek");
+}
+
+module.exports = {testInserts, testSelects, testViewRecs}
--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 const test = require("node:test");
 
-const { testInsert } = require("./test-insert.js");
+const { testInserts } = require("./test-insert.js");
+const {testSelects} = require('./test-insert.js');
+const {testViewRecs} = require('./test-insert.js');
 
 // add a movie recommendation
-test("insertMovie adds a new movie recommendation to the database", testInsert);
+test("insertMovie adds a new movie recommendation to the database", testInserts);
+test("select queries return data from the database", testSelects);
+test("viewRecommendations() lists all recommendations into the home page", testViewRecs);
