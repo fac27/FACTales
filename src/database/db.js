bb242c7023fbc2527d24bbc6da2ef9933d4d2244
simonryrie <simonryrie@gmail.com>
2023-05-24 16:06:04 +0100

    Changed secret environment variable and mount destination

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,9 @@
 const { readFileSync } = require("node:fs");
-const path = require("path");
 const Database = require("better-sqlite3");
 
-const dbFilePath = path.join(__dirname, "db.sqlite");
-const db = new Database(dbFilePath);
+const db = new Database(process.env.DB_FILE);
 
-const schemaFilePath = path.join(__dirname, "schema.sql");
-const schema = readFileSync(schemaFilePath, "utf-8");
+const schema = readFileSync('src/database/schema.sql', "utf-8");
 db.exec(schema);
 
 module.exports = db;
