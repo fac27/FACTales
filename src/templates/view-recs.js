ae220658928098165955a41e9b4fee0220a79da2
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-05-24 12:32:01 +0100

    refactoring

--- src/templates/view-recs.js
+++ src/templates/view-recs.js
@@ -1,23 +1,24 @@
 const { selectMovieRecs } = require("../model/movie-recs.js");
-const { displayBookRecs } = require('../model/books.js');
 
 function viewRecommendations() {
   let recs = selectMovieRecs();
   console.log(recs);
   // if (type === 'movie') method = selectMovieRecs();
-  const recList = recs.map((rec) => 
-    /*HTML*/ `
+  const recList = recs
+    .map(
+      (rec) => /*HTML*/ `
       <li>
-        ${rec.user_name} recommends ${rec.movie_title}
+        ${rec.user_name} recommended ${rec.movie_title}
       </li>
     `
-  ).join('');
-  
+    )
+    .join("");
+
   return /*html*/ `
   <p>
     ${recList}
   </p>
-  `
-};
+  `;
+}
 
 module.exports = viewRecommendations;
