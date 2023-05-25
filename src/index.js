98dd9c4b51525b201760369013b902febaddd46c
simonryrie <simonryrie@gmail.com>
2023-05-25 12:07:29 +0100

    Relaunched - pushing changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const server = require("./server.js")
 
-const port = process.env.PORT || "8080";
+const port = process.env.PORT || "3000";
 
 server.listen(port, () => console.log(`Listening at http://localhost:${port}`))
 
