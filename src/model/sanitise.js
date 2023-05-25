9934389a593af13a7e4b7bfc0cd8227c83a42185
FomasTreeman <tom@team-freeman.com>
2023-05-25 11:45:16 +0100

    fixed eslint errors

--- src/model/sanitise.js
+++ src/model/sanitise.js
@@ -1,10 +1,13 @@
 function sanitise(unsafeData) {
-  let unsafeCharactersObject = {
-    "<": "&lt;",
-    ">": "&gt;",
+  const unsafeCharactersObject = {
+    '<': '&lt;',
+    '>': '&gt;',
   };
 
-  return unsafeData.replace(/<|>/gi, (matched) => unsafeCharactersObject[matched]);
+  return unsafeData.replace(
+    /<|>/gi,
+    (matched) => unsafeCharactersObject[matched]
+  );
 }
 
 module.exports = { sanitise };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
-const test = require("node:test");
-const { assert } = require("node:assert");
+// const test = require("node:test");
+// const { assert } = require("node:assert");
 
-const model = require("../model/routes.js"); // review file name in path
-const db = require("../src/database/db.js");
-const { reset } = require("./test-helper_functions.js");
+// const model = require("../model/routes.js"); // review file name in path
+// const db = require("../src/database/db.js");
+// const { reset } = require("./test-helper_functions.js");
 
 // TEST INSERT ////////////////////////
 //////////////////////////////////////
